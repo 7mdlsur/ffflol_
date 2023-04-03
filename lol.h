427e34db8480f33f3d901e189715a1579a1cc2a7
Jor Hazboun <necroseth@proton.me>
2023-04-04 01:33:11 +0200

    Update lol.h

--- lol.h
+++ lol.h
@@ -13,7 +13,7 @@
 #define ROL(x, y) (((x) << (y)) | ((x) >> (32 - (y))))
 #define A5_STEP(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
 
-void a5_1_decrypt(unsigned char* key, int key_len, unsigned char* cipher, int cipher_len, unsigned char* out);
+void a5_1_decrypt(unsigned char*, int, unsigned char* , int, unsigned char* );
 
 void a5_1_decrypt(unsigned char*, int , unsigned char* , int, unsigned char* );
 
